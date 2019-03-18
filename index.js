e0fb296f940a74a383eef18c44f9a67190bc3e15
Moses-Hamon <moses_hamon@bigpond.com>
2019-03-18 17:22:42 +1000

    Added Puppeteer Example

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("success");
+
+// gumtree screenshot test
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://gumtree.com.au');
+  await page.screenshot({path: 'example2.png'});
+
+  await browser.close();
+})();
