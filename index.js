362925dfe5cbe69d3073190a638db7f174f64e2a
Chris Howard <chrisau233@gmail.com>
2019-03-22 13:27:16 +1000

    add data structure, db data fetching, emailing

--- index.js
+++ index.js
@@ -1,79 +1,146 @@
 
-// gumtree screenshot test
+// package imports
 const puppeteer = require('puppeteer');
 const axios = require('axios');
+const AWS = require('aws-sdk');
 
+// region for SES emailing
+AWS.config.update({ region: 'us-east-1' });
 
-
+// choose AWS credentials profile
+const credentials = new AWS.SharedIniFileCredentials({ profile: 'default' });
+AWS.config.credentials = credentials;
 
 (async () => {
-// variables
+  // variables
   const searchItem = 'Weber';
-  const browser = await puppeteer.launch({headless: false});
-  const page = await browser.newPage();
-const databaseUrl = "http://dbUsername:dbPassword@dbUrl/gumtree-notifier"
+  // const browser = await puppeteer.launch({ headless: false });
+  // const page = await browser.newPage();
+  const databaseUrl = "http://dbUsername:dbPassword@dbUrl/gumtree-notifier/_all_docs?include_docs=true"
   // functions
-  const getOldData = async () => {
+  const getPreviousAdsDataFromDb = async () => {
     try {
-      const response = await axios.get(databaseUrl);
-      console.log(response);
+      const httpResponse = await axios.get(databaseUrl);
+      const previousAdsData = httpResponse.data.rows[0].doc.ads;
+      return previousAdsData;
     } catch (error) {
       console.error(error);
     }
   }
 
-  try {
-
-    // load the page
-    await page.goto('https://gumtree.com.au/');
-    
-    // highlight the search bar
-    await page.focus('.search-query');
-    // input searchItem (weber)
-    await page.keyboard.type(searchItem);
-    // Click the search button
-
-    // Collect Ad information
-
-    
-    await page.evaluate(() => console.log(`The page ${location.href} has been loaded`));
-    await page.screenshot({ path: 'gumtree.png' });
-
-  } catch (error) {
-
-    console.log(`Error: ${error}`);
-    
-  } finally {
-
-    await browser.close();
-
+  const getArrayOfNewAds = (previousAdsData, scrapedAdsData) => {
+    const newAds = scrapedAdsData.filter(scrapedAd => {
+      const scrapedAdId = scrapedAd.id;
+      const previousAdsIds = previousAdsData.map(previousAd => {
+        return previousAd.id
+      })
+      return !previousAdsIds.includes(scrapedAdId)
+    });
+    return newAds;
   }
 
-})();
+  // try {
 
-// put scraped new ads data into data structure
-const newAdsData = {
-  ads: [
+  //   // load the page
+  //   await page.goto('https://gumtree.com.au/');
+
+  //   // highlight the search bar
+  //   await page.focus('.search-query');
+  //   // input searchItem (weber)
+  //   await page.keyboard.type(searchItem);
+  //   // Click the search button
+
+  //   // Collect Ad information
+
+
+  //   await page.evaluate(() => console.log(`The page ${location.href} has been loaded`));
+  //   await page.screenshot({ path: 'gumtree.png' });
+
+  // } catch (error) {
+
+  //   console.log(`Error: ${error}`);
+
+  // } finally {
+
+  //   await browser.close();
+
+  // }
+
+  // put scraped new ads data into data structure
+  const scrapedAdsData = [
     {
-      id: 000000000, // extract id from scraped url
-      url: ""
+      id: 1, // extract id from scraped url
+      url: "http://www.1.com"
     },
     {
-      id: 000000000, // extract id from scraped url
-      url: ""
+      id: 2, // extract id from scraped url
+      url: "http://www.2.com"
     },
     {
-      id: 000000000, // extract id from scraped url
-      url: ""
+      id: 4, // extract id from scraped url
+      url: "http://www.4.com"
     }
   ]
-}
 
-// fetch old ads data from db
+  // fetch old ads data from db
+  const previousAdsData = await getPreviousAdsDataFromDb();
 
+  // check to see if any of the new ads ids are not present in the old ads ids
+  const newAds = getArrayOfNewAds(previousAdsData, scrapedAdsData)
 
-// check to see if any of the new ads ids are not present in the old ads ids
+  const qtyNewAdsFound = newAds.length;
+  const newAdsFound = qtyNewAdsFound > 0;
+  console.log('New ads found: ', qtyNewAdsFound);
 
-// if so send email to ed with the urls from those ads then replace old data in db with new data
+  // if new ads found send email to ed with the urls from those ads
+  if (newAdsFound) {
+    // Create sendEmail params 
+    const params = {
+      Destination: { /* required */
+        // CcAddresses: [
+        // 'example@gmail.com'
+        // ],
+        ToAddresses: [
+          // 'example@gmail.com'
+          'example@gmail.com'
+        ]
+      },
+      Message: { /* required */
+        Body: { /* required */
+          // Html: {
+          //  Charset: "UTF-8",
+          //  Data: "HTML_FORMAT_BODY"
+          // },
+          Text: {
+            Charset: "UTF-8",
+            Data: `Beep boop, new ad big nuts:\n${JSON.stringify(newAds)}`
+          }
+        },
+        Subject: {
+          Charset: 'UTF-8',
+          Data: 'Test email'
+        }
+      },
+      Source: 'example@gmail.com', /* required */
+      ReplyToAddresses: [
+        'example@gmail.com'
+      ],
+    };
 
-// if there are no new ads, dont send email and leave old data in db
+    // Create the promise and SES service object
+    const sendPromise = new AWS.SES({ apiVersion: '2010-12-01' }).sendEmail(params).promise();
+
+    // Handle promise's fulfilled/rejected states
+    sendPromise.then(data => {
+      console.log(data.MessageId);
+    }).catch(err => {
+      console.error(err, err.stack);
+    });
+
+    // if new ads found put the scraped ads array into db (overwrite previous?)
+
+  } // end of 'if (newAdsFound) { ... }'
+
+  // if there are no new ads, dont send email and leave old data in db
+
+})();
